e44bc23cef63240df4b25824246fe477bf9c84f8
Alberto Sanchez del Alamo <a26sanch@uwaterloo.ca>
2024-06-12 18:10:36 +0200

    nb nmkb

--- src/controllers/fileController.js
+++ src/controllers/fileController.js
@@ -23,7 +23,6 @@ const getAllFiles = asyncHandler(async (req, res) => {
 // @route POST /files
 // @access Private
 const uploadFile = asyncHandler(async (req, res) => {});
-const uploadFile = asyncHandler(async (req, res) => {});
 
 // @desc Delete a file
 // @route DELETE /files
